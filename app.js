dd9effd1b92f2a2c41a344e2589087a9f42a060a
d2nwira-cell <d2nwira@gmail.com>
2026-08-01 13:09:25 +0700

    Update app.js
    Properti Channel Telegram -

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // KONFIGURASI -- WAJIB DIISI sebelum dipakai
 // =========================================================
 // Tempel URL deployment Apps Script Anda di sini (yang berakhiran /exec)
-var API_BASE_URL = 'https://script.google.com/macros/s/AKfycbxulPYuYzDJVUuaUDr1tjSqUcjHlW6kDjjZCoA1h7OZEqRxwbteDj8h9I_JXcvGE7kRNw/exec';
+var API_BASE_URL = 'https://script.google.com/macros/s/AKfycbxTbQt0rYnEicJALoYiT5gWAUOYRICiflOMiZd5SzJ-ivo3Ju_NHZiiZt-AWr55XqLz1Q/exec';
 
 // =========================================================
 
